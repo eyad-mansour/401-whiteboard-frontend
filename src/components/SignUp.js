bc9335cb54b5f5c36d364f9041822f3447eae88a
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-09-26 22:07:38 +0300

    add token

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,109 +1,72 @@
-import axios from "axios";
-import React, { useState } from "react";
-import base64 from "base-64";
-import { When } from "react-if";
+import axios from 'axios';
+import React, { useState } from 'react';
+import base64 from 'base-64';
+import cookies from 'react-cookies';
 
-export default function SingUp() {
-  const [loggedin, setLoggedin] = useState(false);
-
+export default function SingUp(props) {
   const handleSignUp = async (e) => {
     e.preventDefault();
     const data = {
       userName: e.target.username.value,
       email: e.target.email.value,
-      password: e.target.password,
+      password: e.target.password.value,
     };
-    await axios.post("http://localhost:3000/signup", data);
+    await axios
+      .post('http://localhost:3000/signup', data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => console.log(e));
+  };
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const data = {
+      username: e.target.email.value,
+      password: e.target.password.value,
+    };
+    const encodedCredintial = base64.encode(
+      `${data.username}:${data.password}`
+    );
+    console.log(encodedCredintial);
+    await axios
+      .post(
+        'http://localhost:3000/login',
+        {},
+        {
+          headers: {
+            Authorization: `Basic ${encodedCredintial}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        cookies.save('token', res.data.token);
+        cookies.save('userID', res.data.id);
+        cookies.save('userName', res.data.userName);
+        props.setLoggedin(true);
+      })
+      .catch((err) => console.log(err));
   };
   return (
-    <section
-      className="vh-100 bg-image"
-      style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');"
-    >
-      <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-        <div className="container h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-              <div className="card" style="border-radius: 15px;">
-                <div className="card-body p-5">
-                  <h2 className="text-uppercase text-center mb-5">
-                    Create an account
-                  </h2>
-
-                  <form action="" onSubmit={handleSignUp}>
-                    <div className="form-outline mb-4">
-                      <input
-                        type="text"
-                        id="form3Example1cg"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="form3Example1cg">
-                        Your Name
-                      </label>
-                    </div>
-
-                    <div className="form-outline mb-4">
-                      <input
-                        type="email"
-                        id="form3Example3cg"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="form3Example3cg">
-                        Your Email
-                      </label>
-                    </div>
-
-                    <div className="form-outline mb-4">
-                      <input
-                        type="password"
-                        id="form3Example4cg"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="form3Example4cg">
-                        Password
-                      </label>
-                    </div>
-
-                    <div className="form-check d-flex justify-content-center mb-5">
-                      <input
-                        className="form-check-input me-2"
-                        type="checkbox"
-                        value=""
-                        id="form2Example3cg"
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="form2Example3g"
-                      >
-                        I agree all statements in{" "}
-                        <a href="#!" className="text-body">
-                          <u>Terms of service</u>
-                        </a>
-                      </label>
-                    </div>
-
-                    <div className="d-flex justify-content-center">
-                      <button
-                        type="button"
-                        className="btn btn-success btn-block btn-lg gradient-custom-4 text-body"
-                      >
-                        Register
-                      </button>
-                    </div>
-
-                    <p className="text-center text-muted mt-5 mb-0">
-                      Have already an account?{" "}
-                      <a href="#!" className="fw-bold text-body">
-                        <u>Login here</u>
-                      </a>
-                    </p>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <>
+      <div>
+        <h2>Sign up</h2>
+        <form action="" onSubmit={handleSignUp}>
+          <input type="text" placeholder="username" name="username" />
+          <input type="email" placeholder="email" name="email" />
+          <input type="text" placeholder="password" name="password" />
+          <button type="submit">Save</button>
+        </form>
       </div>
-    </section>
+
+      <div>
+        <h2>Sign in</h2>
+        <form action="" onSubmit={handleSignIn}>
+          <input type="email" placeholder="email" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <button type="submit">login</button>
+        </form>
+      </div>
+    </>
   );
 }
